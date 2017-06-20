9d4dc1b5ec664c91680c759d460524bc76f26803
JRBotha <jr.botha@outlook.com>
2017-06-20 12:06:41 -0700

    test

---
+++ Project1.rb
@@ -0,0 +1,20 @@
+def caesar_cipher(message, key)
+
+  message.split("").each do |letter|
+    # check if letter is alphabet character
+    if letter.index(/[a-zA-Z]/) != nil
+      convert_letter = letter.ord + key # convert letter to asci and add key
+      #check if converted letter is non alphabet character
+      if (convert_letter.between?(91,96) || convert_letter> 122) 
+        convert_letter -= 26
+      end
+      
+      message[letter] = convert_letter.chr
+    else 
+      message[letter] = letter
+    end
+  end.join("")
+  return message
+end
+
+caesar_cipher("What a string!",5)
