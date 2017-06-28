2de9ad8ac6b18c14aaf7657a621f873d6ea79c4c
JRBotha <jr.botha@outlook.com>
2017-06-28 12:15:00 -0700

    bubble_sort_by function complete

--- bubble_sort_v2.rb
+++ bubble_sort_v2.rb
@@ -8,11 +8,32 @@ def bubble_sort(array)
         end
     end
     
-    return array
+    #return array
     print array
    
 end
 
-bubble_sort([2,4657,1,4,987,2,16,4])      
+   
  
 
+def bubble_sort_by(array)
+    if (!block_given?)
+        return array
+    end
+
+
+    (array.size - 1).times do |cycles|
+
+        (array.size - cycles - 1).times do |i|
+
+            array[i], array[i+1] = array[i+1],array[i] if (yield(array[i], array[i+1]))
+        end
+    end
+
+    #return array
+    print array
+end
+
+bubble_sort_by([2,4657,1,4,987,2,16,4]) {|x,y|x < y}  
+
+    
